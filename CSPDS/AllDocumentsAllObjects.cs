a04f9a2b1af34edd221d7e446514aff8f10d6070
arandomic <it.erand@gmail.com>
2019-11-14 17:38:26 +0300

    search-search

--- CSPDS/AllDocumentsAllObjects.cs
+++ CSPDS/AllDocumentsAllObjects.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Runtime;
@@ -26,134 +27,131 @@ namespace CSPDS
 
         private static ObjectDescriptionNode fromDocument(Document document)
         {
-            ObjectDescriptionNode fileNode = new ObjectDescriptionNode("file:" + document.Name, "");
+            ObjectDescriptionNode fileNode = new ObjectDescriptionNode("file:" + document.Name);
             fileNode.InnerObjects.Add(fromUserData(document.UserData));
 
             var db = document.Database;
             using (Transaction tr = db.TransactionManager.StartTransaction())
             {
-                fileNode.InnerObjects.Add(blockTable(tr, db));
-                fileNode.InnerObjects.Add(layerTable(tr, db));
-                fileNode.InnerObjects.Add(dimTable(tr, db));
                 fileNode.InnerObjects.Add(fromObjectId(db.NamedObjectsDictionaryId, tr));
-                fileNode.InnerObjects.Add(fromObjectId(db.PlotStyleNameDictionaryId, tr));
-                fileNode.InnerObjects.Add(fromObjectId(db.DataLinkDictionaryId, tr));
+                fileNode.InnerObjects.Add(fromObjectId(db.BlockTableId, tr));
+                fileNode.InnerObjects.Add(fromObjectId(db.LayerTableId, tr));
             }
-
             return fileNode;
         }
 
         private static ObjectDescriptionNode fromUserData(Hashtable userdata)
         {
-            ObjectDescriptionNode node = new ObjectDescriptionNode("userdata", "");
+            ObjectDescriptionNode node = new ObjectDescriptionNode("userdata");
             foreach (var key in userdata.Keys)
             {
                 String kv = key.ToString() + ":" + userdata[key].ToString();
-                node.InnerObjects.Add(new ObjectDescriptionNode(kv, ""));
+                node.InnerObjects.Add(new ObjectDescriptionNode(kv));
             }
 
             return node;
+        }
+
+        private static ObjectDescriptionNode fromObject(object obj, Transaction tr)
+        {
+            try
+            {
+                if (obj == null)
+                    return new ObjectDescriptionNode("null", "null");
+
+
+                if (obj is ObjectId)
+                    return fromObjectId((ObjectId) obj, tr);
+                
+                if(obj is SymbolTable)
+                    return fromSymbolTable(obj as SymbolTable, tr);
+                if (obj is IDictionary)
+                    return fromIDictionary(obj as IDictionary, tr);
+                if (obj is IEnumerable)
+                    return fromEnumerable((IEnumerable) obj, tr);
+                if (HasProperty(obj, "Name"))
+                    return fromProperties("named: ", obj, tr);
+                return fromProperties("", obj, tr);
+            }
+            catch (Exception exception)
+            {
+                return new ObjectDescriptionNode("error:" + exception.StackTrace);
+            }
+        }
+
+        public static bool HasProperty(object objectToCheck, string name)
+        {
+            var type = objectToCheck.GetType();
+            return type.GetProperty(name) != null;
+        }
+
+
+        private static ObjectDescriptionNode fromEnumerable(IEnumerable list, Transaction tr)
+        {
+            ObjectDescriptionNode collection = fromProperties("collection: ", list, tr);
+            foreach (var obj in list)
+            {
+                ObjectDescriptionNode childNode = fromObject(obj, tr);
+                collection.InnerObjects.Add(childNode);
+            }
+
+            return collection;
+        }
+        private static ObjectDescriptionNode fromSymbolTable(SymbolTable list, Transaction tr)
+        {
+            ObjectDescriptionNode collection = new ObjectDescriptionNode(list.ToString());
+            foreach (var obj in list)
+            {
+                ObjectDescriptionNode childNode = fromObject(obj, tr);
+                collection.InnerObjects.Add(childNode);
+            }
+
+            return collection;
+        }
+
+        private static ObjectDescriptionNode fromProperties(String prefix, object obj, Transaction tr)
+        {
+            ObjectDescriptionNode objNode = new ObjectDescriptionNode(prefix + obj.GetType().ToString());
+            foreach (var prop in obj.GetType().GetProperties())
+            {
+                ObjectDescriptionNode propNode = new ObjectDescriptionNode("prop = " + prop.Name);
+                String propValue = "nope";
+                try
+                {
+                    propValue = prop.GetValue(obj, null).ToString();
+                }
+                catch (Exception e)
+                {
+                    propValue = "error:" + e.Message;
+                }
+
+                ObjectDescriptionNode valueNode = new ObjectDescriptionNode("value = " +propValue);
+                propNode.InnerObjects.Add(valueNode);
+                objNode.InnerObjects.Add(propNode);
+            }
+
+            return objNode;
         }
 
         private static ObjectDescriptionNode fromObjectId(ObjectId id, Transaction tr)
         {
-            try
-            {
-                var obj = tr.GetObject(id, OpenMode.ForRead);
-
-                if (obj is DBDictionary)
-                    return fromDictionary(obj as DBDictionary, tr);
-//            if (objectType.IsDerivedFrom(RXObject.GetClass(typeof(DBDictionary))))
-//                return fromDictionary(obj as DBDictionary, tr);
-//            RXObject.GetClass(typeof(SymbolTable));
-//            RXObject.GetClass(typeof(SymbolTableRecord));
-                return new ObjectDescriptionNode(obj.ToString(), "");
-            }
-            catch (Exception exception)
-            {
-                return new ObjectDescriptionNode("error:" + exception.Message, "");
-            }
+            var obj = tr.GetObject(id, OpenMode.ForRead);
+            return fromObject(obj, tr);
         }
 
-        private static ObjectDescriptionNode fromDictionary(DBDictionary dict, Transaction tr)
+        private static ObjectDescriptionNode fromIDictionary(IDictionary dict, Transaction tr)
         {
-            ObjectDescriptionNode node =
-                new ObjectDescriptionNode("Dictionary(" + dict.Count + "):" + dict.ToString(), "");
+            ObjectDescriptionNode dictNode = fromProperties("Dictionary: ", dict, tr);
 
-            foreach (var data in dict)
+            foreach (var key in dict.Keys)
             {
-                var key = new ObjectDescriptionNode(data.m_key, "");
-                key.InnerObjects.Add(fromObjectId(data.m_value, tr));
+                var keyNode = new ObjectDescriptionNode(key.ToString());
+                var valueNode = fromObject(dict[key], tr);
+                dictNode.InnerObjects.Add(keyNode);
+                keyNode.InnerObjects.Add(valueNode);
             }
 
-            return node;
-        }
-
-        static ObjectDescriptionNode dimTable(Transaction tr, Database db)
-        {
-            DimStyleTable dt = tr.GetObject(db.DimStyleTableId, OpenMode.ForRead) as DimStyleTable;
-            ObjectDescriptionNode dtNode = new ObjectDescriptionNode(dt.ToString(), "");
-
-            foreach (ObjectId btId in dt)
-            {
-                DimStyleTableRecord dtr = (DimStyleTableRecord) tr.GetObject(btId, OpenMode.ForRead);
-                ObjectDescriptionNode recNode = new ObjectDescriptionNode(dtr.Name, dtr.Name);
-                dtNode.InnerObjects.Add(recNode);
-            }
-
-            return dtNode;
-        }
-
-        static ObjectDescriptionNode layerTable(Transaction tr, Database db)
-        {
-            LayerTable lt = tr.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
-            ObjectDescriptionNode ltNode = new ObjectDescriptionNode(lt.ToString(), "");
-
-            foreach (ObjectId btId in lt)
-            {
-                LayerTableRecord ltr = (LayerTableRecord) tr.GetObject(btId, OpenMode.ForRead);
-                ObjectDescriptionNode recNode = new ObjectDescriptionNode(ltr.Name, ltr.Name);
-
-                ltNode.InnerObjects.Add(recNode);
-            }
-
-
-            return ltNode;
-        }
-
-        static ObjectDescriptionNode blockTable(Transaction tr, Database db)
-        {
-            BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
-            ObjectDescriptionNode btNode = new ObjectDescriptionNode(bt.ToString(), "");
-
-            foreach (ObjectId btrId in bt)
-            {
-                BlockTableRecord btr = (BlockTableRecord) tr.GetObject(btrId, OpenMode.ForRead);
-                ObjectDescriptionNode recNode = new ObjectDescriptionNode(btr.Name, btr.Name);
-                btNode.InnerObjects.Add(recNode);
-
-                foreach (ObjectId id in btr)
-                {
-                    var obj = tr.GetObject(id, OpenMode.ForRead);
-                    ObjectDescriptionNode objNode = new ObjectDescriptionNode(obj.ToString(), "");
-                    recNode.InnerObjects.Add(objNode);
-                }
-            }
-
-            return btNode;
-        }
-
-        static ObjectDescriptionNode namedObjects(Transaction tr, Database db)
-        {
-            ObjectDescriptionNode node = new ObjectDescriptionNode("NamedObjectDicitonary", "");
-            DBDictionary dictionary = tr.GetObject(db.NamedObjectsDictionaryId, OpenMode.ForRead) as DBDictionary;
-
-            foreach (var dictItem in dictionary)
-            {
-                //dictItem
-            }
-
-            return node;
+            return dictNode;
         }
     }
 
@@ -168,6 +166,12 @@ namespace CSPDS
         {
             this.name = name;
             this.description = description;
+        }
+
+        public ObjectDescriptionNode(string name)
+        {
+            this.name = name;
+            this.description = "";
         }
 
         public string Name => name;
--- CSPDS/CommandClass.cs
+++ CSPDS/CommandClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Autodesk.AutoCAD.ApplicationServices.Core;
 
 //Autodesk
@@ -19,7 +20,31 @@ namespace CSPDS
         {
             ObjectDescriptionNode root = AllDocumentsAllObjects.fromAllFiles();
             AllObjectsView treeView = new AllObjectsView(root);
-            Application.ShowModalWindow(treeView);
+            saveToLog(root);
+            Application.ShowModalWindow(treeView);            
         }
+
+        public void saveToLog(ObjectDescriptionNode root)
+        {
+            var frontire = new Stack<Tuple<ObjectDescriptionNode, int>>();
+            frontire.Push(new Tuple<ObjectDescriptionNode, int>(root, 0));
+            
+            using (System.IO.StreamWriter file = 
+                new System.IO.StreamWriter(@"..\objectsTree.log", true))
+            {
+                while (frontire.Count != 0)
+                {
+                    var nextNode = frontire.Pop();
+                    foreach (var child in nextNode.Item1.InnerObjects)
+                    {
+                        frontire.Push(new Tuple<ObjectDescriptionNode, int>(child, nextNode.Item2+1));
+                    }
+                    
+                    file.WriteLine(new String(' ' , nextNode.Item2) + nextNode.Item1.Name);
+                }    
+                
+            }            
+        }
+        
     }
 }
